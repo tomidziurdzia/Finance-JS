d929fc9962616357c66773a5205473577f1f6bf8
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-01-17 20:31:54 -0300

    Formulario modal de cuentas terminado, falta responsive

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ const d = document;
 const iconoMenuUser = d.querySelector("#user__icono");
 const menu = d.querySelector("#user__menu");
 
+const btnNuevaCuenta = d.querySelector("#nueva__cuenta");
+const modalCuenta = d.querySelector("#modal-cuenta");
+
+const btnCerrarFormCuenta = d.querySelector("#modal-form__cerrar");
+
 iconoMenuUser.addEventListener("click", (e) => {
+  e.preventDefault();
   menu.classList.toggle("active");
+});
+
+btnNuevaCuenta.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalCuenta.classList.add("modal-form--show");
+});
+
+btnCerrarFormCuenta.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalCuenta.classList.remove("modal-form--show");
 });
