578efa93a88371ef8f4415dc2f309b1a4b8b6283
Tomi Dziurdzia <tomidziurdzia@hotmail.com>
2022-01-18 09:17:56 -0300

    Termine el formulario para categoria

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@ const iconoMenuUser = d.querySelector("#user__icono");
 const menu = d.querySelector("#user__menu");
 
 const btnNuevaCuenta = d.querySelector("#nueva__cuenta");
+const btnNuevaCategoria = d.querySelector("#nueva__categoria");
 const modalCuenta = d.querySelector("#modal-cuenta");
+const modalCategoria = d.querySelector("#modal-categoria");
 
-const btnCerrarFormCuenta = d.querySelector("#modal-form__cerrar");
+const btnCerrarFormCuenta = d.querySelector("#modal-form__cerrar-cuenta");
+const btnCerrarFormCategoria = d.querySelector("#modal-form__cerrar-categoria");
 
 iconoMenuUser.addEventListener("click", (e) => {
   e.preventDefault();
@@ -18,7 +21,18 @@ btnNuevaCuenta.addEventListener("click", (e) => {
   modalCuenta.classList.add("modal-form--show");
 });
 
+btnNuevaCategoria.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalCategoria.classList.add("modal-form--show");
+});
+
 btnCerrarFormCuenta.addEventListener("click", (e) => {
   e.preventDefault();
   modalCuenta.classList.remove("modal-form--show");
 });
+
+btnCerrarFormCategoria.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalCategoria.classList.remove("modal-form--show");
+});
+
